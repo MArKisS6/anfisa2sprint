600a82e7afffa79178e56dd7bcde165de3aaa7a7
MArKisS6 <playmilokin@gmail.com>
2026-09-06 00:22:34 +0300

    t
    t

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -9,9 +8,8 @@ def index(request):
     ice_cream_list = IceCream.objects.values(
         'id', 'title', 'description'
     ).filter(
-        (Q(is_on_main=True) & Q(is_published=True))
-        | (Q(title__contains='пломбир') & Q(is_published=True))
-    )
+        is_published=True, is_on_main=True
+    ).order_by('title')[:3]
     context = {
         'ice_cream_list': ice_cream_list,
     }
